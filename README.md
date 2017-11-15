6a3fda57e3919ca4715d16424d2fbf125bb75321
John Ojanti Itebu <reemseats@gmail.com>
2017-11-15 07:55:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Shades SCSS 🕶
+
+### Frontend SCSS Boilerplate
+
+Shades SCSS is a light weight, robust, scalable & highly extensible Sass boilerplate for creating css themes. 
+Its very easy to use. All you have to do is set basic values for configuration and compile to create a new theme.
+When used with the configured grunt tasks, you get its full power to;
+- Sass compilation
+- PostCSS : Pixrem, Autoprefixer
+- CSS minification + gzip
+- Create multiple themes
+
+Shades SCSS doesnt get in your way. It gives you just enough to get going and leaves the go crazy with your template designs 🕺
+
+## Get Started
+
+Coming Soon.
+
+## Documentation
+
+Coming Soon.
+
+## License
+
+The code is open source and available under the [MIT License](LICENSE.md).
---
+++ gruntfile.js
@@ -0,0 +1,74 @@
+﻿
+module.exports = function(grunt) {
+
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/build/css/themes/',
+                    src: ['*.scss'],
+                    dest: 'assets/src/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('pixrem')(), // add fallbacks for rem units
+                    require('autoprefixer')({
+                        browsers: ['last 8 versions', 'ie 9', 'ie 8']
+                    }), // add vendor prefixes
+                ]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/src/css/',
+                    src: '*.css',
+                    dest: 'assets/src/css/'
+                }]
+            }
+        },
+        cssmin: {
+            options: {
+                gzip: ['min', 'gzip'],
+                compatibility: 'ie9,-properties.zeroUnits',
+                // sourceMapInlineSources: true,
+                advanced: true,
+            },
+            core: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/src/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'assets/src/css/',
+                    ext: '.min.css'
+                }]
+            },
+        },
+
+    });
+
+    // ===========================================================================
+    // LOAD GRUNT PLUGINS ========================================================
+    // ===========================================================================
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+    // Default task.
+    grunt.registerTask('default', ['sass', 'postcss', 'cssmin']);
+
+};
